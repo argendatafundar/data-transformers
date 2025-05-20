acbb4be502a06cef1cb067983e3f036191269b39
Joan Gonzalez <jgquiroga@dc.uba.ar>
2025-05-20 15:51:08 -0300

    Renombro nombre del paquete

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Data Transformers',
+    name='data-transformers',
     author='Fundar',
     version='0.1',
     packages=find_packages(),
